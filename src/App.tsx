88d4de4194a358b7f38c8c961756a38b192d7a37
hoangm960 <hoangm960@gmail.com>
2025-06-20 15:02:10 +0700

    add toolbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react"
-import Canvas from "./components/Canvas.jsx"
+import Canvas from "./components/Canvas/Canvas.jsx"
+import ToolBar from "./components/ToolBar/ToolBar.jsx"
 
 function App() {
     return (
         <div className="w-full h-full">
             <Canvas></Canvas>
+            <ToolBar></ToolBar>
         </div>
     )
 }
