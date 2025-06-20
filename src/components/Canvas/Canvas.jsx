35a92c224b8823b44e8bc1ed9f39bf8930f3cb20
hoangm960 <hoangm960@gmail.com>
2025-06-20 15:49:15 +0700

    add canvas

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,3 +1,46 @@
+import { useRef, useEffect } from "react"
+
 export default function Canvas() {
-    return <canvas id="whiteboard" className="w-full h-full"></canvas>
+    const canvasRef = useRef(null)
+
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const ctx = canvas.getContext("2d")
+
+        if (canvas) {
+            canvas.width = window.innerWidth
+            canvas.height = window.innerHeight
+
+            const handleResize = () => {
+                canvas.width = window.innerWidth
+                canvas.height = window.innerHeight
+            }
+            window.addEventListener("resize", handleResize)
+        }
+
+        const svgString = `
+          <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="100" cy="100" r="80" fill="orange" stroke="black" stroke-width="5"/>
+          </svg>
+        `
+        const svgBlob = new Blob([svgString], {
+            type: "image/svg+xml;charset=utf-8",
+        })
+        const url = URL.createObjectURL(svgBlob)
+
+        const img = new Image()
+        img.onload = () => {
+            ctx.drawImage(img, 0, 0)
+            URL.revokeObjectURL(url)
+        }
+        img.src = url
+    }, [])
+
+    return (
+        <canvas
+            id="whiteboard"
+            className="bg-gray-950"
+            ref={canvasRef}
+        ></canvas>
+    )
 }
