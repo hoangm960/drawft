a8e426380825e5ec0f366da622dc16554c5b5973
hoangm960 <hoangm960@gmail.com>
2025-06-20 14:51:58 +0700

    cleanup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,29 +1,32 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-  ],
-  ignorePatterns: ["dist", ".eslintrc.cjs", "storybook-static"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "@typescript-eslint"],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
-  rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        "eslint:recommended",
+        "@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
     ],
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "react/prop-types": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/no-explicit-any": "warn",
-  },
-};
+    ignorePatterns: ["dist", ".eslintrc.cjs", "storybook-static"],
+    parser: "@typescript-eslint/parser",
+    plugins: ["react-refresh", "@typescript-eslint"],
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+    rules: {
+        "react-refresh/only-export-components": [
+            "warn",
+            { allowConstantExport: true },
+        ],
+        "@typescript-eslint/no-unused-vars": [
+            "error",
+            { argsIgnorePattern: "^_" },
+        ],
+        "react/prop-types": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-explicit-any": "warn",
+    },
+}
