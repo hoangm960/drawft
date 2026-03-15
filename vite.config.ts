f1eeaec6c46930a86a2790ec8f98b0ace710cf23
hoangm960 <hoangm960@gmail.com>
2026-03-15 22:41:28 +0700

    add deploy workflows

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+    base: process.env.GITHUB_PAGES === "true" ? "/drawft/" : "/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
