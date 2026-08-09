042a994a7b4dbc0d2da55b1db5c403a5e3082b85
hoangm960 <hoangm960@gmail.com>
2026-08-09 21:22:12 +0700

    add useToolStore tests

---
+++ src/stores/__tests__/useToolStore.test.ts
@@ -0,0 +1,13 @@
+import { Tools } from "@/types";
+import { useTool } from "../useToolStore";
+
+describe("useTool", () => {
+    test("starts with the select tool", () => {
+        expect(useTool.getState().tool).toEqual(Tools.select);
+    });
+
+    test("setTool updates the active tool", () => {
+        useTool.getState().setTool(Tools.rect);
+        expect(useTool.getState().tool).toEqual(Tools.rect);
+    });
+});
