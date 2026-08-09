0aa11422a9d05ccfda7809a75da56ff821b1b72e
hoangm960 <hoangm960@gmail.com>
2026-08-09 21:02:52 +0700

    update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,11 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
+import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
+export default defineConfig([
     { ignores: ["dist"] },
     {
         extends: [js.configs.recommended, ...tseslint.configs.recommended],
@@ -28,5 +26,4 @@ export default tseslint.config(
             ],
         },
     },
-    storybook.configs["flat/recommended"]
-);
+]);
